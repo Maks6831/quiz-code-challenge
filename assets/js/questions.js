fa85b58532c5bcd2a02f8045fa1ae377c29b5f9b
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-01-20 18:46:23 +0000

    added questions and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,40 @@
+const quizData = [ 
+    {
+    question: 'How old was Ramana Maharshi at the time of his death?',
+    a: '40',
+    b: '56',
+    c: '79',
+    d: '70', 
+    correct: 'd'
+}, {
+    question: 'What is the most used programming language in 2022?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'Javascript',
+    correct: 'd'
+}, {
+    question: 'Who is the current president of the USA?'
+    a: 'Joe Biden',
+    b: 'Mitt Romney',
+    c: 'Elon Musk',
+    d: 'Donald Trump',
+    correct: 'a'
+}, {
+    question: 'What instrument does Ana Federova play?'
+    a: 'Guitar',
+    b: 'Piano',
+    c: 'Violin',
+    d: 'Organ',
+    correct: 'b'
+}, {
+    question: 'What year was Javascript launched?',
+    a: '2020',
+    b: '2000',
+    c: '1995',
+    d: '1984',
+    correct: 'c'
+
+}
+
+]
