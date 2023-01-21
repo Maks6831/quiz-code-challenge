3a13b23993a158c8558814da43f6a5a7bcb35e4f
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-01-21 19:04:06 +0000

    refactored start quiz eventlistener so it can be reused

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,16 +2,23 @@ let start = document.querySelector("#start");
 let panel = document.querySelector("#questions");
 let question = document.querySelector("#question-title");
 let choiceBox = document.querySelector("#choices");
+const list = document.createElement("ul");
+for (let i = 0; i < quizData[0].choices.length; i++){
+    const li = document.createElement("li")
+    li.className = i;
+    list.appendChild(li);
+}
+
+choiceBox.appendChild(list);
+
+let currentQuest = 0;
+
+
+
+
 
 start.addEventListener("click", function(){
     panel.classList.remove("hide")
-    question.textContent = quizData[0].question
-    const list = document.createElement("ul");
-    for (let i = 0; i < quizData[0].choices.length; i++){
-        const li = document.createElement("li");
-        li.textContent = quizData[0].choices[i];
-        list.appendChild(li);
-
-    }
-    choiceBox.appendChild(list);
+    question.textContent = quizData[currentQuest].question;
+    
 })
