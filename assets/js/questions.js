ea88f392afc175e9eace5bffe13ec83a2c14cf15
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-01-24 17:08:57 +0000

    deleted junk data from questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,49 +3,21 @@ const quizData = [
     question: 'How old was Winston Churchill at the time of his death?',
     choices: ['40', '56', '79', '70'],
     correctChoice: 3,
-    a: '40',
-    b: '56',
-    c: '79',
-    d: '90', 
-    correct: 'd',
-    
 }, {
     question: 'What is the most used programming language in 2022?',
     choices: ['Java', 'C', 'Python', 'Javascript'],
     correctChoice: 3,
-    a: 'Java',
-    b: 'C',
-    c: 'Python',
-    d: 'Javascript',
-    correct: 'd'
 }, {
     question: 'Who is the current president of the USA?',
     choices: ['Joe Biden', 'Mitt Romney', 'Elon Musk', 'Donald Trump'],
     correctChoice: 0,
-    a: 'Joe Biden',
-    b: 'Mitt Romney',
-    c: 'Elon Musk',
-    d: 'Donald Trump',
-    correct: 'a'
 }, {
     question: 'What instrument does Ana Federova play?',
     choices: ['Guitar', 'Piano', 'Violin', 'Organ'],
     correctChoice: 1,
-    a: 'Guitar',
-    b: 'Piano',
-    c: 'Violin',
-    d: 'Organ',
-    correct: 'b'
 }, {
     question: 'What year was Javascript launched?',
     choices: ['2020', '2000', '1995', '1984'],
     correctChoice: 2,
-    a: '2020',
-    b: '2000',
-    c: '1995',
-    d: '1984',
-    correct: 'c'
-
 }
-
 ]
