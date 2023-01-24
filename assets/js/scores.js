75d1f895c7f9c9a602d221be409b43841f18f8e8
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-01-24 17:07:00 +0000

    added comments to scores.js2

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,17 +1,27 @@
-let hs = JSON.parse(localStorage.getItem("high-scores"))||[];
-hs.sort(function(a, b){return b.storedScore - a.storedScore});
-let ol = document.getElementById("highscores");
+//-------------------------------------------- retrieving localstorage data----------------------------------------------------------------//
 
+// variable which retrieve data from the localstorage
+let hs = JSON.parse(localStorage.getItem("high-scores"))||[];
+// sorts array so that the highest score has the lowest index
+hs.sort(function(a, b){return b.storedScore - a.storedScore});
+// links ol from highscore.html
+let ol = document.getElementById("highscores");
+// appends data from local storage to hitml page
 for(let i = 0; i < hs.length; i++){
     let li = document.createElement("li");
+    // adds the initals and score to li 
     li.textContent = hs[i].initials + " - " + hs[i].storedScore;
-
+    //appends data to display in ol
     ol.appendChild(li);
 }
 
+//links clear highscores button to variable clearScores.
 let clearScores = document.getElementById("clear");
 
+// functionality for clear highscores button
 clearScores.addEventListener("click", function(){
+    // removes localStorage data
     localStorage.removeItem("high-scores");
+    // refreshes the page so that ol does not display anything anymore. 
     location.reload();
 })
